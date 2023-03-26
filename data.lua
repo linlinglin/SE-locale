f0a4ab83298f09e269b7bfffe3c387843dcbe126
linlinglin <linlinglin@msn.com>
2023-03-26 08:36:50 +0800

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,36 @@
+data:extend(
+{
+  {
+    type = "sprite",
+    name = "tooltip-category-antimatter",
+    filename = "__SE-locale__/graphics/antimatter.png",
+    priority = "extra-high-no-scale",
+    width = 44,
+    height = 44,
+    flags = {"gui-icon"},
+    mipmap_count = 1,
+    scale = 0.5,
+  },
+   {
+    type = "sprite",
+    name = "tooltip-category-antimatter-fuel",
+    filename = "__SE-locale__/graphics/charged-antimatter-fuel-cell.png",
+    priority = "extra-high-no-scale",
+    width = 64,
+    height = 64,
+    flags = {"gui-icon"},
+    mipmap_count = 4,
+    scale = 0.5,
+  },
+  {
+    type = "sprite",
+    name = "tooltip-category-fusion-fuel",
+    filename = "__SE-locale__/graphics/dt-fuel.png",
+    priority = "extra-high-no-scale",
+    width = 64,
+    height = 64,
+    flags = {"gui-icon"},
+    mipmap_count = 4,
+    scale = 0.5,
+  },  
+})
